855f87a6a4c45ddf453d5a828bc733b04d62a702
FIXED-TERM Szimilko Virag (XC/ENG4-Bp) <fixed-term.virag.szimilko@hu.bosch.com>
2023-03-13 13:23:54 +0100

    fixup! Second week homework

--- HAZI/HAZI02/HAZI02.py
+++ HAZI/HAZI02/HAZI02.py
@@ -6,24 +6,28 @@ def column_swap(input_array) -> np.array:
 
 def compare_two_array(a1, a2) -> np.array:
     compared = a1 == a2
-    return a1[compared]
+    return np.where(compared == True)[0]
 
 def get_array_shape(input_array) -> int:
     return input_array.ndim
 
 def encode_Y(input_array, n) -> np.array:
-    result = np.zeros((n, input_array.max() + 1))
-    result[np.arange(n, input_array)] = 1
+    result = np.zeros(n, input_array.max() + 1)
+    for x in range(result.shape[1]):
+        result[x][input_array[x]] = 1
     return result
 
-def decode_Y() -> np.array:
-    return
+def decode_Y(input_array) -> np.array:
+    result = np.zeros(input_array.shape[1])
+    for x in range(input_array.shape[1]):
+        result[x] = np.where(input_array[x] == 1)[0][0]
+    return result
 
 def eval_classification(str_array, float_array) -> str:
-    max_idx = float_array.index(np.max(float_array))
+    max_idx = np.where(float_array == np.max(float_array))[0][0]
     return str_array[max_idx]
 
-def repalce_odd_numbers(input_array) -> np.array:
+def replace_odd_numbers(input_array) -> np.array:
     input_array[input_array%2 == 1] = -1
     return input_array
 
@@ -39,15 +43,15 @@ def array_multi(input_array) -> int:
     return output
 
 def array_multi_2d(input_array) -> np.array:
-    output = np.array([1,1])
-    for x in range(input_array.ndim):
+    output = np.ones(input_array.shape[0])
+    for x in range(input_array.shape[0]):
         for y in input_array[x]:
             output[x] = output[x] * y
     return output
 
 def add_border(input_array) -> np.array:
-    output = np.array([1,1])
-    return output
+    result = np.zeros(input_array.shape[1] + 2)
+    return result
 
 def list_days(date1, date2) -> list:
     output = []
@@ -57,10 +61,10 @@ def list_days(date1, date2) -> list:
         day = day + 1
     return output
 
-def current_date() -> np.datetime64:
+def get_act_date() -> np.datetime64:
     return np.datetime64('today', 'D')
 
-def sec_from_1970() -> np.timedelta64:
+def sec_from_1970() -> int:
     date_from = np.datetime64('1970-01-01T00:00:00')
     current = np.datetime64('now')
-    return current - date_from
+    return (current - date_from).astype(int)
